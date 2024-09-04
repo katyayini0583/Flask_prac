3974482cee576b0045884186e6a371eb0e783cba
Avijit Roy <avijit.roy@scaler.com>
2024-09-04 22:56:47 +0530

    Flask App

---
+++ predict.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import pickle
+import sklearn
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+model_pickle = open("./artefacts/classifier.pkl", "rb")
+clf = pickle.load(model_pickle)
+
+
+@app.route("/predict", methods=['POST'])
+def prediction():
+    loan_req = request.get_json() 
+    print(loan_req)
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+
+    if loan_req['Married'] == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else: 
+        Credit_History = 1
+
+
+    ApplicantIncome = loan_req['ApplicantIncome'] 
+    LoanAmount = loan_req['LoanAmount'] 
+
+    result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]]) 
+    
+    if result == 0:
+        pred = "Rejected" 
+    else:
+        pred = "Approved"
+    
+    return {"loan_approval_status": pred}
