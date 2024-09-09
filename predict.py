cee20985981df356b4d11fe643066b4217cdddea
Avijit Roy <avijit.roy@scaler.com>
2024-09-09 22:44:24 +0530

    Fixing the changes

--- predict.py
+++ predict.py
@@ -43,7 +43,7 @@ def prediction():
 
     result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]]) 
     
-    if result == 1:
+    if result == 0:
         pred = "Rejected" 
     else:
         pred = "Approved"
